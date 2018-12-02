629186c7c4a992741352dd660852cc1cf81aaf58
Justin Leaming <jtleaming@hotmail.com>
2018-12-01 19:29:19 -0700

    Revert "Adding Spotify Microservice."
    This reverts commit bd6e4714e349d67c50785d63d54fadda4b2dd658.

--- SpotifyMicro/Application/src/Tracks/TracksInfoModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Application.Tracks
-{
-    public class TracksInfoModel
-    {
-        public string Artist { get; set; }
-        public string Tracks { get; set; }
-        public string Uri { get; set; }
-    }
-}
