541558ce66aef12f4e753a80fe24c556f55a181e
Justin Leaming <jtleaming@hotmail.com>
2019-03-10 11:36:53 -0600

    Added docker file for SpotifyMicro. Made some changes to ClientApp Docker and startup in package.

--- SpotifyMicro/Infrastructure/src/SpotifyServices/TrackService.cs
+++ SpotifyMicro/Infrastructure/src/SpotifyServices/TrackService.cs
@@ -11,6 +11,8 @@ namespace Infrastructure.SpotifyServices
     public class TrackService : ITrackService
     {
         private readonly IWebClientWrapper webClientWrapper;
+        private const string spotifyUrl = "https://api.spotify.com/";
+        private const string currentPlayingUri = "v1/me/player/currently-playing";
 
         public TrackService(IWebClientWrapper webClientWrapper)
         {
@@ -19,13 +21,13 @@ namespace Infrastructure.SpotifyServices
 
         public async Task<HttpStatusCode> CheckConnectionAsync()
         {
-            var results = await webClientWrapper.GetAsync("https://api.spotify.com/");
+            var results = await webClientWrapper.GetAsync(spotifyUrl);
             return results.StatusCode;
         }
 
         public async Task<string> GetCurrentTrackAsync()
         {
-            var response = await webClientWrapper.GetAsync("https://api.spotify.com/v1/me/player/currently-playing");
+            var response = await webClientWrapper.GetAsync($"{spotifyUrl}{currentPlayingUri}");
             var content = await response.Content.ReadAsStringAsync();
             return content;
         }
